01be1ca608b0e03346cac4569426665f742a71ba
Sashabrava <sashabrava@yandex.by>
2021-02-11 10:43:24 +0100

    Added URLs for Shop App

--- src/main/java/org/sashabrava/shopserver/controllers/TemplateController.java
+++ src/main/java/org/sashabrava/shopserver/controllers/TemplateController.java
@@ -1,8 +1,10 @@
 package org.sashabrava.shopserver.controllers;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(path = "/")
@@ -10,5 +12,22 @@ public class TemplateController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/api", produces = "application/json")
+
+    public String indexAPI() {
+        JSONObject json = new JSONObject();
+        json.put("text", "Main page of Shop Server is up and running");
+        return json.toString();
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/api/check-alive", produces = "application/json")
+    public String checkAlive() {
+        JSONObject json = new JSONObject();
+        json.put("status", "Running");
+        return json.toString();
     }
 }
